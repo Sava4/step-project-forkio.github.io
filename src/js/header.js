44df3f3d1d6410e8a111613282b6ec784ecedec4
alexandrrusanov <Darkelf3001>
2019-07-28 13:34:22 +0300

    header (section 1 & menu) done// hover added

--- src/js/header.js
+++ src/js/header.js
@@ -5,6 +5,13 @@ burgerMenu.addEventListener('click', () => {
   navMenu.classList.toggle('navMenu-active');
 });
 
+// burgerMenu.addEventListener('mouseenter', (event) => {
+//   console.log(event.target);
+//   if (!navMenu.classList.contains('navMenu-active')) {
+//     navMenu.classList.add('navMenu-active');
+//   }
+// });
+
 window.addEventListener('click', (event) => {
   if (event.target !== burgerMenu && navMenu.classList.contains('navMenu-active')) {
     navMenu.classList.remove('navMenu-active');
